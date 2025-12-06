dfdb4b9d9256e100f44dad2fdbd532ee46e985af
beerbeatbox <beer.beatbox.one@gmail.com>
2025-12-06 09:50:15 +0700

    refactor: Remove unused import of flutter_riverpod from dio_client.dart and onboarding_repository.dart

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
