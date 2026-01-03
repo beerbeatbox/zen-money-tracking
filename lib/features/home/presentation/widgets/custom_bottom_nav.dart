ebb9af37313e4033d5a74c8dd86b95ce7d17c0fe
beerbeatbox <beer.beatbox.one@gmail.com>
2026-01-03 20:32:46 +0700

    fix: Update font weight in CustomBottomNav for active state
    - Changed the font weight for the active state in CustomBottomNav from FontWeight.w500 to FontWeight.w700, enhancing visual emphasis on the active navigation item.

--- lib/features/home/presentation/widgets/custom_bottom_nav.dart
+++ lib/features/home/presentation/widgets/custom_bottom_nav.dart
@@ -20,7 +20,7 @@ class CustomBottomNav extends StatelessWidget {
       overflow: TextOverflow.ellipsis,
       style: TextStyle(
         fontSize: 11,
-        fontWeight: isActive ? FontWeight.w500 : FontWeight.w500,
+        fontWeight: isActive ? FontWeight.w700 : FontWeight.w500,
         color: Colors.black.withValues(alpha: isActive ? 1 : 0.6),
         height: 1.1,
       ),
